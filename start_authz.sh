8de31611cc9d1062254871b7695b01c136cd06ee
Stephan Preeker <stephan@preeker.net>
2019-11-25 10:08:25 +0100

    add seperate authz config stuff

---
+++ start_authz.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+set -u
+set -x
+
+. ./env.sh
+
+# shut down all running services
+docker-compose down
+
+print_update "Up database"
+docker-compose up -d database
+
+print_update "Build authz_admin"
+docker-compose build authz_admin
+
+print_update "Wait for Postgres"
+docker-compose run authz_admin /postgres-wait.sh
+
+print_update "Create database and user for lightidp"
+docker-compose exec database psql -U ${AUTHZ_ADMIN_PG_USER} ${AUTHZ_ADMIN_PG_DB} -c "CREATE DATABASE ${LIGHTIDP_PG_DB};"
+docker-compose exec database psql -U ${AUTHZ_ADMIN_PG_USER} ${AUTHZ_ADMIN_PG_DB} -c "CREATE USER ${LIGHTIDP_PG_USER} WITH PASSWORD '${LIGHTIDP_PG_PASSWORD}';"
+docker-compose exec database psql -U ${AUTHZ_ADMIN_PG_USER} ${AUTHZ_ADMIN_PG_DB} -c "GRANT ALL PRIVILEGES ON DATABASE \"${LIGHTIDP_PG_DB}\" TO ${LIGHTIDP_PG_USER};"
+
+print_update "Prepare database schema for lightidp"
+docker-compose exec database psql -U ${LIGHTIDP_PG_USER} ${LIGHTIDP_PG_DB} -c "CREATE TABLE IF NOT EXISTS users (email character varying(254) PRIMARY KEY, password character varying(128));"
+docker-compose exec database psql -U ${LIGHTIDP_PG_USER} ${LIGHTIDP_PG_DB} -c "INSERT INTO users (email, password) VALUES ('${DEMO_USER_EMAIL}', '${DEMO_USER_PASS_HASH}');"
+
+print_update "Up authz_admin"
+docker-compose up -d authz_admin
+
+print_update "Up lightidp"
+docker-compose up -d lightidp
+
+print_update "Wait for authz_admin"
+docker-compose exec authz_admin /authz_admin-wait.sh
+
+print_update "Up authz"
+docker-compose up -d authz
+
+
